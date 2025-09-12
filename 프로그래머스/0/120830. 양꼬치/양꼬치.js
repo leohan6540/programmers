319749ba6714b8e7166fa413a620b6ce044be797
leohan6540 <36398925+leohan6540@users.noreply.github.com>
2025-09-12 13:54:33 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,4 +1,5 @@
 function solution(n, k) {
-        return n*12000+(k-(Math.floor(n/10)))*2000;
+    return n*12000 + k*2000 - parseInt(n/10)*2000;
+        //return n*12000+(k-(Math.floor(n/10)))*2000;
   
 }
