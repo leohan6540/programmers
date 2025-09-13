0cd9dd6b2f6644fa5461382de9d8be24cbc24d6d
leohan6540 <36398925+leohan6540@users.noreply.github.com>
2025-09-13 20:33:32 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,8 +1,12 @@
+// function solution(numbers) {
+//     let sum=0;
+//     for(let num of numbers) {
+//         sum += num;
+//     }
+//     let answer = (sum/numbers.length) % 0.5 === 0 ? sum/numbers.length : Math.floor(sum/numbers.length);
+//     return answer;
+// }
+
 function solution(numbers) {
-    let sum=0;
-    for(let num of numbers) {
-        sum += num;
-    }
-    let answer = (sum/numbers.length) % 0.5 === 0 ? sum/numbers.length : Math.floor(sum/numbers.length);
-    return answer;
+    return numbers.reduce((a,b) => a +=b,0)/ numbers.length
 }
