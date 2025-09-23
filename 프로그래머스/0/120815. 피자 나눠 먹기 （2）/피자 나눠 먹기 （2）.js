7ecba2d89b35c387f37086676cd908c1f85a2aa1
leohan6540 <36398925+leohan6540@users.noreply.github.com>
2025-09-23 13:43:08 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,11 +1,22 @@
 function solution(n) {
-    //i는 인당 먹어야 하는 조각수
-    for(i = 1; i <= 100; i++) {
-        if(n < 4) {
-            return 1;
-        }else if (n * i % 6 ===0) {
-            return n * i / 6;
+//     //i는 인당 먹어야 하는 조각수
+//     for(i = 1; i <= n*6; i++) {
+//         if(n < 4) {
+//             return 1;
+//         }else if (n * i % 6 ===0) {
+//             return n * i / 6;
+//         }
+//     }
+    
+    let pizza = 1;
+    
+    while(true) {
+        if(pizza * 6 % n === 0) {
+            break
         }
+        pizza++;
+        
     }
     
+    return pizza
 }
