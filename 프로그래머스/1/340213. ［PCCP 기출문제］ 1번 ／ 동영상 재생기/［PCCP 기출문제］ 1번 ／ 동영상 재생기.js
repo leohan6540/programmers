8b25078c4d6f96b390d81f5006b98be13315fa0f
leohan6540 <36398925+leohan6540@users.noreply.github.com>
2026-03-04 11:19:58 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -1,32 +1,29 @@
-function timeToSecond(time) {
-    const [min, sec] = time.split(":").map(Number);
-    return min * 60 + sec;
+function toSec (time) {
+	const [min, sec] = time.split(":").map(Number);
+	return 60 * min + sec
 }
 
-function timeToString(time) {
-    const min = String(Math.floor(time/60)).padStart(2,0);
-    const sec = String(Math.floor(time%60)).padStart(2,0);
-    return `${min}:${sec}`
-    
+function toStr (time) {
+	const min = String(Math.floor(time / 60)).padStart(2, "0")
+	const sec = String(Math.floor(time % 60)).padStart(2, "0");
+	return `${min}:${sec}`
 }
 
-function solution(video_len, pos, op_start, op_end, commands) {
-    let currentSec = timeToSecond(pos);
-    const startSec = timeToSecond(op_start);
-    const endSec = timeToSecond(op_end);
-    const videoLenSec = timeToSecond(video_len)
-    
-    let currentPos = currentSec >= startSec && endSec >= currentSec
-        ? currentSec = endSec
-        : currentSec;
-    
-    commands.forEach((command) => {
-        command === "next" ? currentPos += 10 : currentPos -= 10;
-        currentPos = Math.max(0, currentPos)
-        currentPos = Math.min(currentPos, videoLenSec)
-        if(currentPos >= startSec && endSec >= currentPos) {
-            currentPos = endSec;    
-        }
-    })
-    return timeToString(currentPos)
+function solution (video_len, pos, op_start, op_end, commands) {
+	let curSec = toSec(pos);
+	let vidSec = toSec(video_len);
+	let startSec = toSec(op_start);
+	let endSec = toSec(op_end);
+
+	const skipOp = (time) => time >= startSec && endSec >= time ? endSec : time;
+	curSec= skipOp(curSec)
+
+	commands.forEach((command) => {
+		curSec += (command === "next" ? 10 : -10);
+		curSec = Math.max(0, Math.min(vidSec, curSec));
+		curSec = skipOp(curSec);
+	})
+	return toStr(curSec)
 }
+
+
